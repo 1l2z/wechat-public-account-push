70bd7545534f93607705341c668cda0f5f352d43
1l2z <115409332+1l2z@users.noreply.github.com>
2022-10-11 01:39:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: 'y靖亮',
+      name: '自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCk5R51xcP-oKWrdW1muRTxLNiZY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
